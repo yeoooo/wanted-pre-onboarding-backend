d8cd9990961c8356d2c32060602d6c3bc72b118f
yeoooo <ruisya330@gmail.com>
2023-08-07 23:20:36 +0900

    chore: 파일명 변경
    Board -> Post

--- src/main/java/com/wanted/subject/domain/board/Post.java
+++ src/main/java/com/wanted/subject/domain/board/Post.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @Table(name = "Boards")
 @Entity
-public class Board {
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
