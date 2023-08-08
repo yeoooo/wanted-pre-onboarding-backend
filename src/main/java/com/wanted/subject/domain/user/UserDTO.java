5a7aa54ce0d688f08fef8b7d8b696b44a40d6cd4
yeoooo <ruisya330@gmail.com>
2023-08-08 23:05:05 +0900

    feat : UserDTO 필드 추가
    기본 생성자, Getter 추가, Entity -> DTO 객체 변환 메서드 추가

--- src/main/java/com/wanted/subject/domain/user/UserDTO.java
+++ src/main/java/com/wanted/subject/domain/user/UserDTO.java
@@ -1,9 +1,21 @@
 package com.wanted.subject.domain.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     @Email
     private String email;
 
     @Password
     private String password;
+
+    public static UserDTO toDTO(User user) {
+        return new UserDTO(user.getEmail(), user.getPassword());
+    }
 }
